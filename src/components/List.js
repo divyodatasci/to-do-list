efc12a0694ffe6c28cec472de0a5d3b2bce62a22
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2024-03-08 00:44:32 +0000

    Adds feature to clear tasks

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const List = ({taskList}) => {
+
+const List = ({taskList, clearTask}) => {
+  // const [tasks,setTasks] = useState(taskList);
+  // const handleClear = (id) => {
+  //   setTasks(tasks.filter(item => item.id !== id));
+  //   console.log(id)
+  // }
+  const handleClear = (id) => {
+    clearTask(id);
+  }
 
   return (
     <div className=' m-4'>
-        {taskList.map((item, index) => {
-            return <div key={index}> {item} </div>;
-        })}
-
-
+      <ul>
+        {taskList.map((item) => {
+              return <li key={item.id}>
+                  <div>
+                    <span className=' text-lg'>{item.task}</span> 
+                    <span className='mx-4'> <FontAwesomeIcon icon={faTimes} className=' cursor-pointer' onClick={() => handleClear(item.id)}/> </span>
+                  </div>
+                 </li>;
+          })}
+      </ul>
     </div>
   )
 }
--- src/components/TaskAdder.js
+++ src/components/TaskAdder.js
@@ -3,13 +3,32 @@ import List from './List';
 import { demoTaskList } from '../utils/democontent';
 const TaskAdder = () => {
   const inputRef = useRef(null);
-  const [taskList, setTaskList] = useState(demoTaskList);
+  const taskObjList = demoTaskList.map((item, index) => {
+    return {
+      id: index,
+      task: item
+    };
+  })
+
+  const getUniqueId = (objectList) => {
+    const maxId = objectList.sort((a,b) => a.id - b.id).pop().id + 1;
+    return maxId;
+  } 
+   
+  const [taskList, setTaskList] = useState(taskObjList);
   const submitHandler = (e) => {
     e.preventDefault();
-    //console.log(inputRef.current.value)
-    setTaskList([inputRef.current.value, ...taskList])
+    const newTask = {
+      id: getUniqueId(taskList),
+      item: inputRef.current.value
+    }
+    setTaskList([newTask, ...taskList])
+    console.log(taskList)
     inputRef.current.value = '';
 
+  }
+  const taskClearer = (taskId) => {
+    setTaskList(taskList.filter(item => item.id !== taskId));
   }
   return (
     <div className=' m-4'>
@@ -19,7 +38,7 @@ const TaskAdder = () => {
             <button className=" m-2 bg-gray-950 text-white p-2 rounded-lg" type='submit'> Add </button>
         </form>
 
-      <List taskList = {taskList} />
+      <List taskList = {taskList} clearTask = {taskClearer}/>
     </div>
   )
 }
