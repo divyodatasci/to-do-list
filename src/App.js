bc7c9e6f02034a87141937bef8fe1de7c65a7d78
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2024-04-10 00:28:44 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -4,38 +4,34 @@ import TaskAdderRedux from './components/TaskAdderRedux';
 import {Provider} from "react-redux";
 import './index.css'
 import appStore from './utils/appStore';
+import Header from './components/Header';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 
 function App() {
-  const taskList = [
-    "Complete homework assignments",
-    "Call mom",
-    "Buy groceries",
-    "Schedule dentist appointment",
-    "Go for a run",
-    "Read a book",
-    "Write a blog post",
-    "Organize closet",
-    "Attend meeting with team",
-    "Cook dinner",
-    "Practice coding",
-    "Water plants",
-    "Pay bills",
-    "Clean the house",
-    "Watch a documentary",
-    "Plan weekend getaway",
-    "Update resume",
-    "Research new recipes",
-    "Take out the trash",
-    "Review project proposal"
-  ];
   return (
     <Provider store={appStore} >
       <div className="App">
-        <TaskAdder/>
-        <TaskAdderRedux/>
+        <Header />
+        <Outlet/>
       </div>
     </Provider>
   );
 }
+export const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children:[
+      {
+        path:'/',
+        element:<TaskAdder/>
+      }, 
+      {
+        path: '/taskAdderRedux',
+        element: <TaskAdderRedux/>
+      }
+    ]
+  }
+])
 
 export default App;
